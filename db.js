bae456f808cf377bff14f6d75d946da8907cfdb9
Tymek <tymoteusz.luczko@hyd.agency>
2021-06-19 10:39:05 +0200

    build test js

---
+++ db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  posts: [
+    { id: 1, title: "json-server", author: "Puma" },
+    { id: 2, title: "json-server-test", author: "Puma" },
+  ],
+  comments: [{ id: 1, body: "some comment", postId: 1 }],
+  profile: { name: "Puma" },
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const db = require("./db.js");
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
